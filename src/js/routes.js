9ddff5c6b762add7eec1fcd883d0dfb73ba1f7d2
david <davidcruz2806@gmail.com>
2020-05-25 20:14:27 -0500

    Paginas

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,10 +1,20 @@
 
 import HomePage from '../pages/home.f7.html';
+import Postres from '../pages/postres.f7.html';
+import Sopas from '../pages/sopas.f7.html';
 
 var routes = [
   {
     path: '/',
     component: HomePage,
+  },
+    {
+    path: '/postres/',
+    component: Postres,
+  },
+    {
+    path: '/sopas/',
+    component: Sopas,
   },
 ];
 
